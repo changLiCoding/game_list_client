ed25f689ec1b7210f936771a19f82b5fb7dc3a8b
Viet Tran <viettran101294@gmail.com>
2023-04-18 18:44:53 -0400

    Add codegen config file

---
+++ codegen.ts
@@ -0,0 +1,20 @@
+import { CodegenConfig } from "@graphql-codegen/cli";
+import dotenv from "dotenv";
+dotenv.config();
+
+const config: CodegenConfig = {
+  schema: process.env.REACT_APP_BACKEND!,
+  documents: ["src/**/*.tsx", "src/**/*.ts"],
+  generates: {
+    "./src/graphql/__generated__/": {
+      preset: "client",
+      plugins: [],
+      presetConfig: {
+        gqlTagName: "gql",
+      },
+    },
+  },
+  ignoreNoDocuments: true,
+};
+
+export default config;
