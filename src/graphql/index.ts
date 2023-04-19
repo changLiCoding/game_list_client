caf8d3856458e15b7ee4f8c9591150a9c3c68cb4
Viet Tran <viettran101294@gmail.com>
2023-04-18 21:12:42 -0400

    Import env file through vite

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const apolloClient = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  uri: import.meta.env.VITE_BACKEND,
   cache: new InMemoryCache(),
 });
