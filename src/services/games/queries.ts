117b0489e5d1d51ba85084905a658c3d93ceaa5c
Yzma <10090913+Yzma@users.noreply.github.com>
2023-04-24 16:50:20 -0400

    Added TypedDocumentNode to give a return type of Game[] for GET_ALL_GAMES query

--- src/services/games/queries.ts
+++ src/services/games/queries.ts
@@ -1,7 +1,9 @@
 import gql from "graphql-tag";
+import { TypedDocumentNode } from "@graphql-typed-document-node/core";
+import { Game } from "../../graphql/__generated__/graphql";
 
-export const GET_ALL_GAMES = gql`
-  query {
+export const GET_ALL_GAMES: TypedDocumentNode<Game[], void> = gql`
+  query GetAllGames {
     allGames {
       id
       name
