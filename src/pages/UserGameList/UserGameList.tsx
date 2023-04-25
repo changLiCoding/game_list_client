9eadaf1fe662a1ad2514615791981ca16f9c8332
Viet Tran <viettran101294@gmail.com>
2023-04-25 12:28:37 -0400

    Add table to display user game list

---
+++ src/pages/UserGameList/UserGameList.tsx
@@ -0,0 +1,84 @@
+import { Table } from "antd";
+
+import "./UserGameListStyle.css";
+import useUserGames from "../../services/userGames/useUserGames";
+import { Game } from "../../graphql/__generated__/graphql";
+import { ColumnsType, TableProps } from "antd/es/table";
+
+type DataType = Game & {
+  key: React.Key;
+};
+
+const columns: ColumnsType<DataType> = [
+  {
+    title: "Name",
+    dataIndex: "name",
+  },
+  {
+    title: "Average Score",
+    dataIndex: "avgScore",
+    sorter: {
+      compare: (a, b) => a.avgScore - b.avgScore,
+      multiple: 3,
+    },
+  },
+  // {
+  //   title: "Math Score",
+  //   dataIndex: "math",
+  //   sorter: {
+  //     compare: (a, b) => a.math - b.math,
+  //     multiple: 2,
+  //   },
+  // },
+  // {
+  //   title: "English Score",
+  //   dataIndex: "english",
+  //   sorter: {
+  //     compare: (a, b) => a.english - b.english,
+  //     multiple: 1,
+  //   },
+  // },
+];
+
+const UserGameList = () => {
+  // info("Cannot load the list of games");
+  const { gamesForAUserLoading, gamesForAUser } = useUserGames();
+
+  if (gamesForAUserLoading) {
+    return <div>Loading...</div>;
+  }
+
+  const onChange: TableProps<DataType>["onChange"] = (
+    pagination,
+    filters,
+    sorter,
+    extra
+  ) => {
+    console.log("params", pagination, filters, sorter, extra);
+  };
+
+  const data = gamesForAUser?.gamesForAUser.map((val: Game) => {
+    return {
+      key: val.id,
+      ...val,
+    };
+  });
+
+  console.log(gamesForAUser?.gamesForAUser);
+
+  return (
+    <>
+      <div className="table-container">
+        <Table
+          className="table"
+          columns={columns}
+          dataSource={data}
+          onChange={onChange}
+        />
+      </div>
+      {/* {contextHolder} */}
+    </>
+  );
+};
+
+export default UserGameList;
