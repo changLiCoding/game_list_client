9aec083b2e326eb40074bd20a3128e1922c448a7
javascriptsucks <evix1988@live.cn>
2023-04-25 01:07:52 -0600

    test for gameForAUser passed

--- src/services/userGames/useUserGames.ts
+++ src/services/userGames/useUserGames.ts
@@ -1,15 +1,14 @@
-import { useMutation } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import {
 	AddUserGamesPayload,
 	DeleteUserGamesPayload,
-	Game,
 } from "../../graphql/__generated__/graphql";
 import { ADD_USER_GAMES, DELETE_USER_GAMES, GAMES_FOR_A_USER } from "./queries";
 
 const useUserGames = () => {
 	const [addUserGamesRequest] = useMutation(ADD_USER_GAMES);
 	const [deleteUserGamesRequest] = useMutation(DELETE_USER_GAMES);
-	const [gamesForAUserRequest] = useMutation(GAMES_FOR_A_USER);
+	const { loading, data: gamesForAUser } = useQuery(GAMES_FOR_A_USER);
 
 	const addUserGames = async (gameId: Number): Promise<AddUserGamesPayload> => {
 		try {
@@ -63,27 +62,6 @@ const useUserGames = () => {
 		}
 	};
 
-	const gamesForAUser = async (): Promise<Array<Game>> => {
-		try {
-			const response = await gamesForAUserRequest();
-			if (
-				!response ||
-				!response.data ||
-				!response.data.gamesForAUser ||
-				response.data.gamesForAUser.errors[0]
-			) {
-				console.log(
-					"gamesForAUser from useUserGames error: ",
-					response.data.gamesForAUser.errors[0]
-				);
-				throw new Error(response.data.gamesForAUser.errors[0]);
-			}
-			console.log("gamesForAUser response: ", response);
-			return response.data.gamesForAUser;
-		} catch (error: any) {
-			return error && { errors: [error.message] };
-		}
-	};
 	return {
 		addUserGames,
 		deleteUserGames,
