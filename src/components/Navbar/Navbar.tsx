7ea83c1aac948a6777383d45f14dc7907a918e06
Yzma <10090913+Yzma@users.noreply.github.com>
2023-04-26 16:40:04 -0400

    Added basic ant-design menu as Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { useState } from 'react';
+import { AppstoreOutlined, MailOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
 
-const Navbar = () => {
-  return <div>Navbar</div>;
-};
+import style from './Navbar.module.css'
+import type { MenuProps } from 'antd';
 
-export default Navbar;
+export default function Navbar() {
+  const items: MenuProps['items'] = [
+    {
+      label: 'Home',
+      key: 'home',
+    },
+    {
+      label: 'Profile',
+      key: 'mail',
+      icon: <MailOutlined />,
+    },
+    {
+      label: 'Game List',
+      key: 'app',
+      icon: <AppstoreOutlined />,
+      disabled: true,
+    },
+  ];
+
+  const [current, setCurrent] = useState('mail');
+
+  const onClick: MenuProps['onClick'] = (e) => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  };
+
+  return (
+    <Menu className={style.root} onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
+  )
+}
+
