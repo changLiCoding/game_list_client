358cd03af763f8d85e496576e6695cf40b4ed3b3
Viet Tran <viettran101294@gmail.com>
2023-05-01 02:56:12 -0400

    Added test for get game list for a user

--- src/tests/UserGames/gamesForAUser.test.tsx
+++ src/tests/UserGames/gamesForAUser.test.tsx
@@ -1,20 +1,63 @@
-// Tests for the gamesForAUser query
+import { render, screen } from '@testing-library/react';
+import { vi } from 'vitest';
+import UserGameList from '@/pages/UserGameList/UserGameList';
+import ContextWrapper from '@/ContextWrapper';
 
-import { apolloClient } from '@/graphql';
-import { GAMES_FOR_A_USER } from '@/services/userGames/queries';
+vi.mock('../../services/userGames/useUserGames', async () => {
+  const actual: unknown = await vi.importActual(
+    '../../services/userGames/useUserGames'
+  );
+  if (typeof actual !== 'object')
+    throw new Error('Import Actual did not return not an object');
+  return {
+    ...actual,
+    default: () => ({
+      gamesForAUserLoading: false,
+      gamesForAUser: {
+        gamesForAUser: [
+          {
+            avgScore: 6.9,
+            description: 'Maiores cupiditate ut. Qui et tempore.',
+            genres: ['Survival', 'Trivia'],
+            id: '26',
+            imageURL: 'https://loremflickr.com/300/300/game',
+            name: 'Mega Man Battle Network',
+            platforms: ['PlayStation Portable', 'PlayStation Vita'],
+            releaseDate: '2017-04-25T00:00:00Z',
+            tags: ['Singleplayer', 'Fantasy'],
+            __typename: 'Game',
+          },
+          {
+            avgScore: 1.7,
+            description: 'Aut ab accusantium. Molestiae ut ea.',
+            genres: ['Platformer', 'Survival horror'],
+            id: '27',
+            imageURL: 'https://loremflickr.com/300/300/game',
+            name: 'Halo 2',
+            platforms: ['PlayStation', 'Sega Dreamcast'],
+            releaseDate: '2019-08-01T00:00:00Z',
+            tags: ['2D', 'Adventure'],
+            __typename: 'Game',
+          },
+        ],
+      },
+    }),
+  };
+});
+
+const { getComputedStyle } = window;
+window.getComputedStyle = (elt) => getComputedStyle(elt);
 
 describe('Get all games for a user', () => {
-  it('should return all games for a user with token', async () => {
-    const query = await apolloClient.query({
-      query: GAMES_FOR_A_USER,
-      context: {
-        headers: {
-          Authorization: `Bearer ${import.meta.env.VITE_TOKEN_TEST}`,
-        },
-      },
-    });
-
-    expect(query.data.gamesForAUser).toBeTruthy();
-    expect(query.data.gamesForAUser[1].name).toEqual('Halo 2');
+  it('should render all games for a user', async () => {
+    render(
+      <ContextWrapper>
+        <UserGameList />
+      </ContextWrapper>
+    );
+    const gameElements = screen.queryAllByText('Halo 2');
+    expect(gameElements[0].textContent).toBe('Halo 2');
+    const avgScoreElements = screen.queryAllByText('1.7');
+    expect(avgScoreElements[0].textContent).toBe('1.7');
   });
 });
