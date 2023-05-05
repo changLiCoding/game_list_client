3755605c76703d35b40c9d8d7f8af792c9a34efb
javascriptsucks <evix1988@live.cn>
2023-05-05 17:18:06 -0600

    change test expectation from variable name into actual strings

--- src/tests/GameDetailPage.test/GameDetailHeaderComponent/GameDetailHeaderComponent.test.tsx
+++ src/tests/GameDetailPage.test/GameDetailHeaderComponent/GameDetailHeaderComponent.test.tsx
@@ -3,10 +3,11 @@ import { render, screen } from '@testing-library/react';
 
 import ContextWrapper from '@/ContextWrapper';
 import GameDetailHeaderInfo from '@/components/GameDetailHeader/GameDetailHeaderInfo/GameDetailHeaderInfo';
+import GameDetailHeaderBanner from '@/components/GameDetailHeader/GameDetailHeaderBanner';
 import { Game } from '@/graphql/__generated__/graphql';
 
-describe('GameDetailHeaderInfo', () => {
-  it('should render GameDetailHeaderInfo with name and description', () => {
+describe('GameDetailHeader', () => {
+  it('should render GameDetailHeader with banner and info', () => {
     const game: Game = {
       __typename: 'Game',
       id: '1',
@@ -22,14 +23,21 @@ describe('GameDetailHeaderInfo', () => {
     };
     render(
       <ContextWrapper>
+        <GameDetailHeaderBanner game={game} />
         <GameDetailHeaderInfo game={game} />
       </ContextWrapper>
     );
 
-    const nameElement = screen.getByText(game.name);
+    const bannerElement = screen.getByRole('banner');
+    expect(bannerElement).toBeInTheDocument();
+    expect(bannerElement).toHaveStyle(
+      `background-image: url(https://example.com/banner.jpg)`
+    );
+
+    const nameElement = screen.getByText('Game 1');
     expect(nameElement).toBeInTheDocument();
 
-    const descriptionElement = screen.getByText(game.description);
+    const descriptionElement = screen.getByText('Description 1');
     expect(descriptionElement).toBeInTheDocument();
   });
 });
