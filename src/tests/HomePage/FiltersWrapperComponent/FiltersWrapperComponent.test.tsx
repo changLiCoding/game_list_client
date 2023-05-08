b805f64476d3eed56cda01deba8e6b913a5af32c
Viet Tran <viettran101294@gmail.com>
2023-05-07 23:42:52 -0400

    Fix test for Render Filters Wrapper Component in Tablet Screen

--- src/tests/HomePage/FiltersWrapperComponent/FiltersWrapperComponent.test.tsx
+++ src/tests/HomePage/FiltersWrapperComponent/FiltersWrapperComponent.test.tsx
@@ -5,8 +5,10 @@ import userEvent from '@testing-library/user-event';
 import ContextWrapper from '@/ContextWrapper';
 import FiltersWrapper from '@/components/FiltersWrapper';
 
-vi.mock('../../services/game/useGame', async () => {
-  const actual: unknown = await vi.importActual('../../services/game/useGame');
+vi.mock('../../../services/game/useGame', async () => {
+  const actual: unknown = await vi.importActual(
+    '../../../services/game/useGame'
+  );
   if (typeof actual !== 'object')
     throw new Error('Import Actual did not return not an object');
   return {
@@ -46,32 +48,62 @@ vi.mock('../../services/game/useGame', async () => {
   };
 });
 
+vi.mock('antd', async () => {
+  const actual: unknown = await vi.importActual('antd');
+  if (typeof actual !== 'object')
+    throw new Error('Import Actual did not return not an object');
+  return {
+    ...actual,
+    Grid: {
+      useBreakpoint: vi.fn().mockReturnValue({
+        xs: false,
+        sm: false,
+        md: true,
+        lg: false,
+        xl: false,
+        xxl: false,
+      }),
+    },
+  };
+});
+
 describe('Filters Wrapper Component', () => {
   it('Render Filters Wrapper Component in Tablet Screen', async () => {
-    vi.spyOn(window.screen, 'width', 'get').mockReturnValue(768);
-    render(
+    const { queryByText, queryAllByText } = render(
       <ContextWrapper>
         <FiltersWrapper />
       </ContextWrapper>
     );
-    waitFor(() => {
-      expect(screen.getByText('Genres')).toBeInTheDocument();
-      const genres = screen.getAllByPlaceholderText('Genre');
-      userEvent.click(genres[0]);
-      expect(screen.getByText('Genre 1')).toBeInTheDocument();
-      expect(screen.getByText('Genre 2')).toBeInTheDocument();
 
-      expect(screen.getByText('Tags')).toBeInTheDocument();
-      const tags = screen.getAllByPlaceholderText('Tag');
-      userEvent.click(tags[0]);
-      expect(screen.getByText('Tag 1')).toBeInTheDocument();
-      expect(screen.getByText('Tag 2')).toBeInTheDocument();
+    expect(queryAllByText('Genres')[0]).toBeInTheDocument();
 
-      expect(screen.getByText('Platforms')).toBeInTheDocument();
-      const platforms = screen.getAllByPlaceholderText('Platform');
-      userEvent.click(platforms[0]);
-      expect(screen.getByText('Platform 1')).toBeInTheDocument();
-      expect(screen.getByText('Platform 2')).toBeInTheDocument();
+    const genres = screen.getByTestId('dropdown-Genres')
+      .firstElementChild as Element;
+    await userEvent.click(genres);
+
+    await waitFor(() => {
+      expect(queryByText('Genre 1')).toBeInTheDocument();
+      expect(queryByText('Genre 2')).toBeInTheDocument();
+    });
+
+    expect(queryAllByText('Tags')[0]).toBeInTheDocument();
+
+    const tags = screen.getByTestId('dropdown-Tags')
+      .firstElementChild as Element;
+    await userEvent.click(tags);
+
+    await waitFor(() => {
+      expect(queryByText('Tag 1')).toBeInTheDocument();
+      expect(queryByText('Tag 2')).toBeInTheDocument();
+    });
+
+    const platforms = screen.getByTestId('dropdown-Platforms')
+      .firstElementChild as Element;
+    await userEvent.click(platforms);
+
+    await waitFor(() => {
+      expect(queryByText('Platform 1')).toBeInTheDocument();
+      expect(queryByText('Platform 2')).toBeInTheDocument();
     });
   });
 });
