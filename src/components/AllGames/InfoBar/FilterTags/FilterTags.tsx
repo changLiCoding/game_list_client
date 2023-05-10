a8192651d80bfaa78a0d2991448a855434177930
javascriptsucks <evix1988@live.cn>
2023-05-10 01:24:54 -0600

    styled infoBar SCSS with hover and icons

--- src/components/AllGames/InfoBar/FilterTags/FilterTags.tsx
+++ src/components/AllGames/InfoBar/FilterTags/FilterTags.tsx
@@ -1,16 +1,27 @@
 import { TagsTwoTone } from '@ant-design/icons';
+import { Tag } from 'antd';
 
 import styles from '@/components/AllGames/InfoBar/FilterTags/FilterTags.module.scss';
 
 function FilterTags() {
+  const log = (e: React.MouseEvent<HTMLElement>) => {
+    // console.log(e);
+  };
   return (
     <div className={styles.tagsContainer}>
-      <TagsTwoTone
-        style={{
-          fontSize: '1.5rem',
-          transform: 'scale(-1) rotate(90deg)',
-        }}
-      />
+      <TagsTwoTone className={styles.tagsIcon} />
+      <Tag closable onClose={log} className={styles.tagsText}>
+        Dark Souls
+      </Tag>
+      <Tag closable onClose={log} className={styles.tagsText}>
+        Dark Souls
+      </Tag>
+      <Tag closable onClose={log} className={styles.tagsText}>
+        Dark Souls
+      </Tag>
+      <Tag closable onClose={log} className={styles.tagsText}>
+        Dark Souls
+      </Tag>
     </div>
   );
 }
--- src/components/AllGames/InfoBar/SelectorsWrapper/SelectorsWrapper.tsx
+++ src/components/AllGames/InfoBar/SelectorsWrapper/SelectorsWrapper.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import {
+  EllipsisOutlined,
+  AppstoreFilled,
+  UnorderedListOutlined,
+  MoreOutlined,
+} from '@ant-design/icons';
+import styles from '@/components/AllGames/InfoBar/SelectorsWrapper/SelectorsWrapper.module.scss';
 
 function SelectorsWrapper() {
-  return <div>SelectorsWrapper</div>;
+  return (
+    <div className={styles.selectorsContainer}>
+      <div className={styles.sortSelector}>
+        {/* <MoreOutlined /> */}
+        <EllipsisOutlined />
+        <span>Rating</span>
+      </div>
+      <div className={styles.wrapper}>
+        <AppstoreFilled className={styles.selectorIcon} />
+        <UnorderedListOutlined className={styles.selectorIcon} />
+      </div>
+    </div>
+  );
 }
 
 export default SelectorsWrapper;
