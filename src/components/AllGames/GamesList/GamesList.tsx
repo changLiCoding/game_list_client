7a05006fa138f2175ffd7a0a2065549292b07b8d
javascriptsucks <evix1988@live.cn>
2023-05-11 17:40:29 -0600

    made tags and title all hiden and ellipsis

--- src/components/AllGames/GamesList/GamesList.tsx
+++ src/components/AllGames/GamesList/GamesList.tsx
@@ -6,6 +6,7 @@ import List from '@/components/AllGames/GamesList/List';
 import useAllGames from '@/services/games/useAllGames';
 
 import styles from '@/components/AllGames/GamesList/GamesList.module.scss';
+import { Game as GameType } from '@/graphql/__generated__/graphql';
 
 export default function GamesList({ isCardView }: { isCardView: boolean }) {
   const { games } = useAllGames();
@@ -39,13 +40,17 @@ export default function GamesList({ isCardView }: { isCardView: boolean }) {
         <div className={styles.allListContainer}>
           <div className={styles.allListTitle}>All Games</div>
           <div className={styles.allListDivider}>
-            {games.map((game) => (
-              <List
-                key={game.id}
-                game={game}
-                colorBgContainer={colorBgContainer}
-              />
-            ))}
+            {[...games]
+              .sort((a, b) => {
+                return parseInt(a.id, 10) - parseInt(b.id, 10);
+              })
+              .map((game) => (
+                <List
+                  key={game.id}
+                  game={game}
+                  colorBgContainer={colorBgContainer}
+                />
+              ))}
           </div>
         </div>
       )}
--- src/components/AllGames/GamesList/List/List.tsx
+++ src/components/AllGames/GamesList/List/List.tsx
@@ -1,5 +1,6 @@
 import { Tag } from 'antd';
 import { FrownOutlined, MehOutlined, SmileOutlined } from '@ant-design/icons';
+import Color from 'color-thief-react';
 
 import { Game as GameType } from '@/graphql/__generated__/graphql';
 import styles from '@/components/AllGames/GamesList/List/List.module.scss';
@@ -42,67 +43,85 @@ function List({
     );
   };
   return (
-    <div
-      className={styles.gameListContainer}
-      style={{ backgroundColor: `${colorBgContainer}` }}
+    <Color
+      crossOrigin="anonymous"
+      src={
+        game.imageURL
+          ? game.imageURL
+          : 'https://images.igdb.com/igdb/image/upload/t_cover_big/co4a7a.png'
+      }
+      format="hex"
     >
-      <div className={styles.gameRankNumber}>
-        <span className={styles.gameRankHash}>#</span>
-        {game.id}
-      </div>
-      <a
-        href={`/game-detail/${game.id}/${game.name}`}
-        className={styles.gameLink}
-      >
-        {game.imageURL && (
-          <img
-            src={game?.imageURL}
-            className={styles.gameImage}
-            alt={game.name}
-          />
-        )}
-      </a>
-      <div className={styles.gameContent}>
-        <div className={styles.gameTitle}>
-          <div>
-            <a href={`/game-detail/${game.id}/${game.name}`}>{game.name}</a>
+      {({ data, error }) => (
+        <div
+          className={styles.gameListContainer}
+          style={{ backgroundColor: `${colorBgContainer}` }}
+        >
+          <div className={styles.gameRankNumber}>
+            <span className={styles.gameRankHash}>#</span>
+            {game.id}
           </div>
-          <div className={styles.gameGenres}>
-            {game.genres.map((genre: string) => (
-              <Tag
-                bordered={false}
-                color="geekblue"
-                key={`${game.name}-${genre}`}
-              >
-                {genre}
-              </Tag>
-            ))}
+          <a
+            href={`/game-detail/${game.id}/${game.name}`}
+            className={styles.gameLink}
+          >
+            {game.imageURL && (
+              <img
+                src={game?.imageURL}
+                className={styles.gameImage}
+                alt={game.name}
+              />
+            )}
+          </a>
+          <div className={styles.gameContent}>
+            <div className={styles.gameTitle}>
+              <div>
+                <a href={`/game-detail/${game.id}/${game.name}`}>{game.name}</a>
+              </div>
+              <div className={styles.gameGenres}>
+                {game.genres.map((genre: string) => (
+                  <Tag
+                    bordered={false}
+                    color={data}
+                    key={`${game.name}-${genre}`}
+                  >
+                    {genre}
+                  </Tag>
+                ))}
+              </div>
+            </div>
+            <div className={styles.gameRating}>
+              {game.avgScore && getRatingIcon(game.avgScore, '#91caff')}
+              <div>
+                Rating: {game.avgScore}
+                <div>99999 users</div>
+              </div>
+            </div>
+            <div className={styles.gamePlatforms}>
+              {game.platforms.map((platform: string) => (
+                <Tag
+                  key={`${platform}-${game.name}`}
+                  className={styles.gamePlatform}
+                  color="geekblue"
+                  style={{
+                    marginBottom: '5px',
+                    maxWidth: '125px',
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                  }}
+                >
+                  {platform}
+                </Tag>
+              ))}
+            </div>
+            <div className={styles.gameReleaseDate}>
+              <div>Release Date: </div>
+              {new Date(game.releaseDate).toISOString().slice(0, 10)}
+            </div>
           </div>
         </div>
-        <div className={styles.gameRating}>
-          {game.avgScore && getRatingIcon(game.avgScore, '#91caff')}
-          <div>
-            Rating: {game.avgScore}
-            <div>99999 users</div>
-          </div>
-        </div>
-        <div className={styles.gamePlatforms}>
-          {game.platforms.map((platform: string) => (
-            <Tag
-              key={`${platform}-${game.name}`}
-              className={styles.gamePlatform}
-              color="geekblue"
-              style={{ maxWidth: '90px', overflow: 'hidden' }}
-            >
-              {platform}
-            </Tag>
-          ))}
-        </div>
-        <div className={styles.gameReleaseDate}>
-          {new Date(game.releaseDate).toISOString().slice(0, 10)}
-        </div>
-      </div>
-    </div>
+      )}
+    </Color>
   );
 }
 
