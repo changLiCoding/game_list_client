eb9b354099cbffdee0e20c4caceb4598d6b08ab8
javascriptsucks <evix1988@live.cn>
2023-05-11 08:32:38 -0600

    basic styles for List components

--- src/components/AllGames/GamesList/List/List.tsx
+++ src/components/AllGames/GamesList/List/List.tsx
@@ -1,4 +1,5 @@
 import { Game as GameType } from '@/graphql/__generated__/graphql';
+import styles from '@/components/AllGames/GamesList/List/List.module.scss';
 
 function List({
   game,
@@ -7,7 +8,30 @@ function List({
   game: GameType;
   colorBgContainer: string;
 }): JSX.Element {
-  return <div>List</div>;
+  return (
+    <div
+      className={styles.gameListContainer}
+      style={{ backgroundColor: `${colorBgContainer}` }}
+    >
+      <div className={styles.gameRankNumber}>
+        <span className={styles.gameRankHash}>#</span>
+        {game.id}
+      </div>
+      <a
+        href={`/game-detail/${game.id}/${game.name}`}
+        className={styles.gameLink}
+      >
+        {game.imageURL && (
+          <img
+            src={game?.imageURL}
+            className={styles.gameImage}
+            alt={game.name}
+          />
+        )}
+      </a>
+      <div className={styles.gameContent}>{game.name}</div>
+    </div>
+  );
 }
 
 export default List;
