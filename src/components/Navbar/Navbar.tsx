9603736117c5ef1d94c1736350e5c4921a1a57e0
Viet Tran <viettran101294@gmail.com>
2023-05-16 22:10:31 -0400

    Fixed error printed on console due to Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -156,11 +156,12 @@ export default function Navbar() {
                   <ul className={styles['desktop-nav__nav-section']}>
                     {!loading && userState?.user?.username ? (
                       <Popover content={content}>
-                        <Image
+                        <img
+                          className={styles['desktop-nav__profile-image']}
                           data-testid="profile-image"
                           width={38}
                           height={38}
-                          preview={false}
+                          alt="profile"
                           src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
                         />
                       </Popover>
