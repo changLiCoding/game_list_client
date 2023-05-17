bdf2bf141ce18ee9aa942961cf7e328cc387e771
Yzma <10090913+Yzma@users.noreply.github.com>
2023-05-17 18:35:22 -0400

    Created homeSearchSlice to store search filters

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,10 +2,12 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import userReducer from '@/features/userSlice';
 import userGamesListReducer from '@/features/userUserGamesListSlice';
+import homeSearchSlice from '@/features/homeSearchSlice';
 
 const rootReducer = combineReducers({
   user: userReducer,
   userGames: userGamesListReducer,
+  homeSearch: homeSearchSlice,
 });
 
 export const store = configureStore({
