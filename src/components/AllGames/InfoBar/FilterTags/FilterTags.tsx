2e23f7cf08324ccbb01dc064cfdf0f771b32386b
Yzma <10090913+Yzma@users.noreply.github.com>
2023-05-17 21:49:12 -0400

    Implemenet filters that auto update when selecting

--- src/components/AllGames/InfoBar/FilterTags/FilterTags.tsx
+++ src/components/AllGames/InfoBar/FilterTags/FilterTags.tsx
@@ -1,8 +1,11 @@
 import { TagsTwoTone } from '@ant-design/icons';
 import { Tag } from 'antd';
+import { useDispatch } from 'react-redux';
 import styles from '@/components/AllGames/InfoBar/FilterTags/FilterTags.module.scss';
 import type { OnChangeCascaderType } from '@/types/global';
 import type { FilterTagsType } from '@/components/AllGames/InfoBar/types';
+import { useAppSelector } from '@/app/hooks';
+import { clearAll, removeFilter } from '@/features/homeSearchSlice';
 
 function FilterTags({ tagsArr, setTagsArr }: FilterTagsType) {
   const handleClose = (removedTag: string | OnChangeCascaderType) => {
@@ -10,20 +13,69 @@ function FilterTags({ tagsArr, setTagsArr }: FilterTagsType) {
     setTagsArr(newTags);
   };
 
+  const dispatch = useDispatch();
+  const homeSearchState = useAppSelector((state) => state.homeSearch);
+  const filtersLength =
+    homeSearchState.filters.genres.length +
+    homeSearchState.filters.platforms.length +
+    homeSearchState.filters.tags.length;
   return (
     <div className={styles.tagsContainer}>
-      {tagsArr.length > 0 && <TagsTwoTone className={styles.tagsIcon} />}
-      {tagsArr &&
-        tagsArr.map((tag) => (
+      {filtersLength > 0 && (
+        <>
+          {' '}
+          <TagsTwoTone className={styles.tagsIcon} />
+          {homeSearchState.filters.genres.map((filter) => {
+            return (
+              <Tag
+                closable
+                onClose={() =>
+                  dispatch(removeFilter({ type: 'Genre', value: filter }))
+                }
+                key={filter}
+                className={styles.tagsText}
+              >
+                {filter}
+              </Tag>
+            );
+          })}
+          {homeSearchState.filters.platforms.map((filter) => {
+            return (
+              <Tag
+                closable
+                onClose={() =>
+                  dispatch(removeFilter({ type: 'Platform', value: filter }))
+                }
+                key={filter}
+                className={styles.tagsText}
+              >
+                {filter}
+              </Tag>
+            );
+          })}
+          {homeSearchState.filters.tags.map((filter) => {
+            return (
+              <Tag
+                closable
+                onClose={() =>
+                  dispatch(removeFilter({ type: 'Tag', value: filter }))
+                }
+                key={filter}
+                className={styles.tagsText}
+              >
+                {filter}
+              </Tag>
+            );
+          })}
           <Tag
             closable
-            onClose={() => handleClose(tag.value)}
-            key={tag.id}
+            onClose={() => dispatch(clearAll())}
             className={styles.tagsText}
           >
-            {tag.value}
+            Clear all
           </Tag>
-        ))}
+        </>
+      )}
     </div>
   );
 }
