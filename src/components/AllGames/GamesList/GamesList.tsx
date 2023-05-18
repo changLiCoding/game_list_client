9b7f9d795c23ec86a076d569756eb7eba6873b2d
Yzma <10090913+Yzma@users.noreply.github.com>
2023-05-18 13:01:31 -0400

    Fix useEffect and types

--- src/components/AllGames/GamesList/GamesList.tsx
+++ src/components/AllGames/GamesList/GamesList.tsx
@@ -26,7 +26,7 @@ export default function GamesList({ isCardView }: { isCardView: boolean }) {
     client.refetchQueries({
       include: [GET_ALL_GAMES],
     });
-  }, [homeSearchState.filters]);
+  }, [client, homeSearchState.filters]);
 
   const {
     token: { colorBgContainer },
