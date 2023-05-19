c3c1ba9be25986ba8310fe0c35d18f952d14ded6
javascriptsucks <evix1988@live.cn>
2023-05-19 01:10:55 -0600

    fix phoneSmall UserProfile page overflow hidden and fontsize

--- src/components/ProfileContent/Overview/MainSection/ListStatistic/ListStatistic.tsx
+++ src/components/ProfileContent/Overview/MainSection/ListStatistic/ListStatistic.tsx
@@ -1,21 +1,29 @@
-import { Col, Row, Statistic, Divider } from 'antd';
+import { Col, Grid, Row, Statistic, Divider } from 'antd';
 
 import styles from '@/components/ProfileContent/Overview/MainSection/ListStatistic/ListStatistic.module.scss';
 import type { UserGamesByStatus } from '@/graphql/__generated__/graphql';
+
+const { useBreakpoint } = Grid;
 
 function ListStatistic({
   gamesByStatus,
 }: {
   gamesByStatus?: UserGamesByStatus;
 }) {
+  const screens = useBreakpoint();
+
   const gameStatusExtractor = (gamesObjData: UserGamesByStatus) => {
     const result: JSX.Element[] = [];
+
     Object.keys(gamesObjData).forEach((key) => {
       if (key.includes('Count')) {
         result.push(
           <Col span={4} key={key}>
             <Statistic
-              valueStyle={{ color: '#b368e6', fontSize: '14px' }}
+              valueStyle={{
+                color: '#b368e6',
+                fontSize: `${screens.xs ? '10px' : '14px'}`,
+              }}
               title={key.replace('Count', '').toUpperCase()}
               value={
                 gamesObjData[
