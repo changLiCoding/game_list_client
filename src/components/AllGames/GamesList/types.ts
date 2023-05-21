e4606779136db45fdf896bd8385ef4a302dd90de
Viet Tran <viettran101294@gmail.com>
2023-05-20 20:43:35 -0400

    Refactor ListEditor out of GameCard

--- src/components/AllGames/GamesList/types.ts
+++ src/components/AllGames/GamesList/types.ts
@@ -1,18 +1,8 @@
-import type { OperationVariables, QueryResult } from '@apollo/client';
-import type {
-  Game,
-  GetUserGameByGameIdQuery,
-} from '@/graphql/__generated__/graphql';
+import type { Game } from '@/graphql/__generated__/graphql';
 
 export type GameCardType = {
   game: Game;
   colorBgContainer: string;
   userGameLoading?: boolean;
-  fetchUserGame?: ({
-    variables,
-  }: {
-    variables: {
-      gameId: string;
-    };
-  }) => Promise<QueryResult<GetUserGameByGameIdQuery, OperationVariables>>;
+  openGameListEditor?: (game: Game) => void;
 };
