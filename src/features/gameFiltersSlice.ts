d0751ebd04797d9a5be7e5e70c16096d468b8985
Yzma <10090913+Yzma@users.noreply.github.com>
2023-05-25 18:33:42 -0400

    Cleanup commnets and implemented resetFilter function in gameFilterSlice

--- src/features/gameFiltersSlice.ts
+++ src/features/gameFiltersSlice.ts
@@ -1,79 +1,5 @@
-import { PayloadAction, createAction, createSlice } from '@reduxjs/toolkit';
-import { GameFilters } from '@/graphql/__generated__/graphql';
-import { remove } from '@/utils/utils';
-
-// type FiltersWithLastSelected = {
-//   [K in keyof Filters]: {
-//     value: Filters[K];
-//     lastSelected?: Filters[K] extends Array<infer T> ? T : Filters[K];
-//   };
-// };
-
-// type GetFiltersType<K extends FilterKeys> = Filters[K] extends Array<infer T>
-//   ? T
-//   : Filters[K];
-
-// export function withPayloadType<K extends keyof Filters>(
-//   key: K,
-//   value: GetFiltersType<K>
-// ) {
-//   // Dispatch the action with the key and value
-//   // Example implementation:
-// }
-// function test() {
-//   withPayloadType('genres', []);
-//   withPayloadType('platforms', 2);
-//   withPayloadType('year', 21);
-// }
-
-// export function typeTest<T extends keyof Filters, K extends Filters[T]>(
-//   key: T,
-//   value: K
-// ) {}
-
-// function testCode() {
-//   typeTest('genres', []);
-//   typeTest('platforms', []);
-//   typeTest('year', 21);
-//   typeTest('tags', []);
-//   // Fails
-//   typeTest('year', '21');
-//   typeTest('tags', [3]);
-// }
-
-// function setFilter<T extends FilterKeys, K extends Filters[T]>(
-//   filter: T,
-//   value: K
-// ) {
-//   return {
-//     type: 'SET_FILTER',
-//     payload: {
-//       filter,
-//       value,
-//     },
-//   };
-// }
-
-// type SetFilterType<T extends keyof Filters> = {
-//   key: T;
-//   value: Filters[T];
-// };
-
-// function withPayloadType<T>() {
-//   return (t: T) => ({ payload: t });
-// }
-// createAction('SET_FILTER', withPayloadType<SetFilterType>());
-
-type Filters = Omit<GameFilters, '__typename' | 'errors'>;
-type FilterKeys = keyof Filters;
-
-type FiltersWithUndefined = {
-  [K in keyof Filters]: Filters[K] | undefined;
-};
-
-type GetFiltersType<T extends FilterKeys> = T extends keyof Filters
-  ? Filters[T] | undefined
-  : never;
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { FiltersWithUndefined } from '@/types/global';
 
 const initialState: FiltersWithUndefined = {
   genres: [],
@@ -93,35 +19,10 @@ export const gameFiltersSlice = createSlice({
       return { ...state, ...action.payload };
     },
 
-    // setFilter22: <T extends keyof Filters>(
-    //   state: Filters,
-    //   action: PayloadAction<{ key: T; value: GetFiltersType<T> }>
-    // ) => {
-    //   const { key, value } = action.payload;
-    //   state[key] = value;
-    // },
-
-    // removeFilter: <T extends keyof Filters>(
-    //   state: Filters,
-    //   action: PayloadAction<{ key: T; value: GetFiltersType<T> }>
-    // ) => {
-    //   const { key, value } = action.payload;
-    //   const t = state[key];
-
-    //   if (Array.isArray(t)) {
-    //     remove(t, value);
-    //   } else {
-    //     state[key] = initialState[key];
-    //   }
-    // },
-
-    // resetFilter: <T extends keyof Filters>(
-    //   state: Filters,
-    //   action: PayloadAction<{ key: T; value: GetFiltersType<T> }>
-    // ) => {
-    //   const { key } = action.payload;
-    //   state[key] = initialState[key];
-    // },
+    resetFilter: (state, action: PayloadAction<keyof FiltersWithUndefined>) => {
+      const filterKey = action.payload;
+      return { ...state, [filterKey]: initialState[filterKey] };
+    },
 
     reset() {
       return initialState;
@@ -129,10 +30,6 @@ export const gameFiltersSlice = createSlice({
   },
 });
 
-export const { setFilters } = gameFiltersSlice.actions;
+export const { setFilters, resetFilter, reset } = gameFiltersSlice.actions;
 
 export default gameFiltersSlice.reducer;
-
-// const filtersReducer = homeSearchSlice.reducer;
-
-// export { setFilter, filtersReducer };
