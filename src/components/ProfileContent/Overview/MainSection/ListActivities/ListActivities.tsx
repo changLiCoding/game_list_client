89157296a2274c4cb0b6d2e6f13e6186f9ca1571
javascriptsucks <evix1988@live.cn>
2023-05-29 17:46:01 -0600

    fix some styles in List Rating component, created barebones PostInput component

--- src/components/ProfileContent/Overview/MainSection/ListActivities/ListActivities.tsx
+++ src/components/ProfileContent/Overview/MainSection/ListActivities/ListActivities.tsx
@@ -1,3 +1,7 @@
+import { DownOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Dropdown, Space } from 'antd';
+
 import styles from '@/components/ProfileContent/Overview/MainSection/ListActivities/ListActivities.module.scss';
 import useStatusUpdates from '@/services/statusUpdate/useStatusUpdates';
 import { StatusUpdate as StatusUpdateType } from '@/graphql/__generated__/graphql';
@@ -8,10 +12,37 @@ function ListActivities() {
   const { statusUpdates, loading } = useStatusUpdates();
   statusUpdates && console.log(statusUpdates);
 
+  const items: MenuProps['items'] = [
+    {
+      label: 'All',
+      key: '0',
+    },
+    {
+      label: 'List',
+      key: '1',
+    },
+    {
+      label: 'Posts',
+      key: '3',
+    },
+  ];
+
   return (
     <div className={styles.listActivitiesContainer}>
       {loading && <div>Loading...</div>}
-      <h2 className={styles.title}>Activities</h2>
+      <h2 className={styles.title}>
+        Activities
+        <Dropdown
+          overlayClassName="dropdownFilter"
+          menu={{ items }}
+          trigger={['click']}
+          arrow
+        >
+          <Space>
+            Filter <DownOutlined />
+          </Space>
+        </Dropdown>
+      </h2>
       <PostInput />
       <ActivitiesUpdates />
     </div>
--- src/components/ProfileContent/Overview/MainSection/ListActivities/PostInput/PostInput.tsx
+++ src/components/ProfileContent/Overview/MainSection/ListActivities/PostInput/PostInput.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import { Input } from 'antd';
+
+import styles from '@/components/ProfileContent/Overview/MainSection/ListActivities/PostInput/PostInput.module.scss';
 
 function PostInput() {
-  return <div>PostInput</div>;
+  return (
+    <div className={styles.postInputContainer}>
+      <Input size="large" placeholder="Post Something" />
+    </div>
+  );
 }
 
 export default PostInput;
