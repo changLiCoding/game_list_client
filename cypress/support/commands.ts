5df657d3c7a8ac71be253fdbea03037a7c95b5e3
Yzma <10090913+Yzma@users.noreply.github.com>
2023-06-04 12:18:20 -0400

    Fix type errors for cypress

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -11,7 +11,7 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+// Cypress.Commands.add('login', (email, password) => {  })
 //
 //
 // -- This is a child command --
