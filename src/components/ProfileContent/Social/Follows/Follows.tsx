abd712136415fb82ad84023b9eb6c5379d4923c6
javascriptsucks <evix1988@live.cn>
2023-06-05 10:56:09 -0600

    implement modal confirm when click unfollow user

--- src/components/ProfileContent/Social/Follows/Follows.tsx
+++ src/components/ProfileContent/Social/Follows/Follows.tsx
@@ -1,8 +1,10 @@
 import { CloseOutlined } from '@ant-design/icons';
+import { Modal } from 'antd';
 
 import styles from '@/components/ProfileContent/Social/Follows/Follows.module.scss';
 import { User as UserType } from '@/graphql/__generated__/graphql';
 import useAddRemoveFollow from '@/services/follows/useAddRemoveFollow';
+import useNotification from '@/hooks/useNotification';
 
 function Follows({
   follows,
@@ -16,6 +18,27 @@ function Follows({
   selectedFilter: string;
 }) {
   const { addFollow, removeFollow } = useAddRemoveFollow();
+
+  const { success, contextHolder, warning } = useNotification();
+
+  const handleRemoveFollow = (followedUser: UserType) => {
+    Modal.confirm({
+      title: `Are you sure you want to unfollow ${followedUser.username}?`,
+      content: 'You will no longer see their posts in your feed.',
+      onOk: async () => {
+        const response = await removeFollow(followedUser.id);
+
+        if (response?.follow && response?.errors?.length === 0) {
+          success(`You have unfollowed ${followedUser.username} successfully.`);
+        } else {
+          warning(
+            `There is something wrong when processing unfollow ${followedUser.username}!`
+          );
+        }
+      },
+    });
+  };
+
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -34,11 +57,12 @@ function Follows({
               <a href={`/user/${follow.username}/`}>{follow.username}</a>
             </div>
             <div className={styles.unfollow}>
-              <CloseOutlined onClick={async () => removeFollow(follow.id)} />{' '}
+              <CloseOutlined onClick={() => handleRemoveFollow(follow)} />{' '}
             </div>
           </div>
         ))}
       </div>
+      {contextHolder}
     </div>
   );
 }
