875f7e5a05ab0644dff8f2045e6d6e313fa338d2
javascriptsucks <evix1988@live.cn>
2023-06-15 15:56:58 -0600

    use readFragment to read cache data from allGames fetch

--- src/pages/GameDetail/GameDetail.tsx
+++ src/pages/GameDetail/GameDetail.tsx
@@ -1,22 +1,40 @@
 import { useParams } from 'react-router-dom';
+import { gql } from '@apollo/client';
 import { Layout } from 'antd';
 import { Content } from 'antd/es/layout/layout';
 import type { Game as GameType } from '@/graphql/__generated__/graphql';
 import useAllGames from '@/services/games/useAllGames';
+import { GET_ALL_GAMES } from '@/services/games/queries';
 import GameDetailHeader from '@/components/GameDetailHeader';
+import { apolloClient } from '@/graphql';
 
 function GameDetail() {
   const { id } = useParams();
-  const {
-    games: gamesState,
-    loading,
-  }: { games: GameType[]; loading: boolean } = useAllGames();
 
-  if (loading) return <div>Loading...</div>;
+  const game = apolloClient.readFragment({
+    id: `Game:${id}`,
+    fragment: gql`
+      fragment GetAllGames on Game {
+        id
+        name
+        description
+        bannerURL
+        imageURL
+        releaseDate
+        avgScore
+        totalRating
+        genres
+        tags
+        platforms
+        isGameAdded
+      }
+    `,
+  });
+  console.log(id, game);
 
-  const game = gamesState.find((gameEle: GameType) => gameEle.id === id);
+  // const game = gamesState.find((gameEle: GameType) => gameEle.id === id);
 
-  return game !== undefined ? (
+  return game ? (
     <Layout>
       <Content>
         <GameDetailHeader game={game} />
