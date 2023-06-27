7a6e6fc53dd4880b8a96a6c8796f518ea9227d68
javascriptsucks <evix1988@live.cn>
2023-06-27 09:01:17 -0600

    create container for all FavoritesCard in Favorites Component

--- src/components/ProfileContent/Favorites/Favorites.tsx
+++ src/components/ProfileContent/Favorites/Favorites.tsx
@@ -1,20 +1,28 @@
 import useGetLikedGames from '@/services/like/useGetLikedGames';
+import styles from '@/components/ProfileContent/Favorites/Favorites.module.scss';
 
 function Favorites() {
   const { likedGames, loading } = useGetLikedGames();
 
   return (
-    <div>
+    <div className={styles.container}>
       Favorites
       {loading ? (
         <div>Loading...</div>
       ) : (
-        <div>
-          {likedGames.map((like) => (
-            <div key={like.id}>
-              {like.likeable.__typename === 'Game' ? like.likeable?.name : null}
+        <div className={styles.favorites}>
+          <div className={styles.favorites}>
+            <h2>games</h2>
+            <div className={styles.favoritesContainer}>
+              {likedGames.map((like) => (
+                <div key={like.id}>
+                  {like.likeable.__typename === 'Game'
+                    ? like.likeable?.name
+                    : null}
+                </div>
+              ))}
             </div>
-          ))}
+          </div>
         </div>
       )}
     </div>
