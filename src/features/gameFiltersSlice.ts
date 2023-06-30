b5cbd29c475bb31bce9cb91a725761937b2304e0
Yzma <10090913+Yzma@users.noreply.github.com>
2023-06-29 21:38:25 -0400

    Cleaned up comments for gameFilterSlice

--- src/features/gameFiltersSlice.ts
+++ src/features/gameFiltersSlice.ts
@@ -6,57 +6,6 @@ import {
   createSlice,
 } from '@reduxjs/toolkit';
 
-// export type GenericGameFilterOptions<T, R extends SliceCaseReducers<T>> = {
-//   name: string;
-//   initialState: T;
-//   reducers: ValidateSliceCaseReducers<T, R>;
-// };
-
-// export function createGameFiltersSlice<
-//   T,
-//   Reducers extends SliceCaseReducers<T>
-// >({ name, initialState, reducers }: GenericGameFilterOptions<T, Reducers>) {
-//   return createSlice({
-//     name,
-//     initialState,
-//     reducers: {
-//       setFilters: (state: Draft<T>, action: PayloadAction<Partial<T>>) => {
-//         return { ...state, ...action.payload };
-//       },
-
-//       resetFilter: (state: Draft<T>, action: PayloadAction<keyof T>) => {
-//         const filterKey = action.payload;
-//         return { ...state, [filterKey]: initialState[filterKey] };
-//       },
-
-//       reset: () => initialState,
-
-//       ...reducers,
-//     },
-//   });
-// }
-
-// const wrappedSlice = createGameFiltersSlice({
-//   name: 'test',
-//   initialState: {
-//     t: '',
-//   },
-//   reducers: {
-//     magic(state) {
-//       state.t = '';
-//     },
-//   },
-// });
-
-// export type GenericGameFilterOptions<
-//   T,
-//   Reducers extends SliceCaseReducers<T>
-// > = {
-//   name: string;
-//   initialState: T;
-//   reducers: ValidateSliceCaseReducers<T, Reducers>;
-// };
-
 export function createGameFiltersSlice<
   T,
   Reducers extends SliceCaseReducers<T>
@@ -88,15 +37,3 @@ export function createGameFiltersSlice<
     },
   });
 }
-
-const wrappedSlice = createGameFiltersSlice({
-  name: 'test',
-  initialState: {
-    t: '',
-  },
-  reducers: {
-    magic(state) {
-      state.t = '';
-    },
-  },
-});
