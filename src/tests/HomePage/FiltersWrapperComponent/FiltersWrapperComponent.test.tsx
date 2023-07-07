9a1091aad636962d4d96691b2f71e9b590defc10
javascriptsucks <evix1988@live.cn>
2023-07-07 12:31:40 -0600

    try to fix test in FiltersWrapper

--- src/tests/HomePage/FiltersWrapperComponent/FiltersWrapperComponent.test.tsx
+++ src/tests/HomePage/FiltersWrapperComponent/FiltersWrapperComponent.test.tsx
@@ -4,26 +4,106 @@ import userEvent from '@testing-library/user-event';
 
 import ContextWrapper from '@/ContextWrapper';
 import FiltersWrapper from '@/components/FiltersWrapper';
+import { store } from '@/app/store';
+import { GET_GAME_FILTERS } from '@/services/game/queries';
+import { GET_ALL_GAMES } from '@/services/games/queries';
+import { DefaultMockedProvider, renderVite } from '@/utils/test-utils';
 
 // TODO: Rewrite this test when new design is implemented
 
-vi.mock('../../../services/game/useGetFilters', async () => {
-  const actual: unknown = await vi.importActual(
-    '../../../services/game/useGetFilters'
-  );
-  if (typeof actual !== 'object')
-    throw new Error('Import Actual did not return not an object');
-  return {
-    ...actual,
-    default: () => ({
-      genres: ['Genre 1', 'Genre 2'],
-      platforms: ['Platform 1', 'Platform 2'],
-      tags: ['Tag 1', 'Tag 2'],
-      year: 2025,
-      errors: [],
-    }),
-  };
-});
+const mocks = [
+  {
+    request: {
+      query: GET_ALL_GAMES,
+      variables: {
+        limit: 20,
+        genre: [],
+        tag: [],
+        platform: [],
+        year: undefined,
+        sortBy: 'name',
+        search: '',
+        offset: 0,
+      },
+    },
+    result: {
+      data: {
+        allGames: [
+          {
+            __typename: 'Game',
+            id: '1',
+            name: 'Game 1',
+            description: 'Description 1',
+            imageURL:
+              'https://images.igdb.com/igdb/image/upload/t_cover_big/co4a7a.png',
+            bannerURL:
+              'https://images.igdb.com/igdb/image/upload/t_cover_big/co4a7a.png',
+            tags: ['3D', 'Fantasy'],
+            genres: ['Action', 'Adventure'],
+            platforms: ['PC', 'macOS'],
+            releaseDate: '2021-01-01 00:00:00',
+            avgScore: 5,
+            totalRating: 5,
+            isGameAdded: false,
+            isGameLiked: false,
+          },
+          {
+            __typename: 'Game',
+            id: '2',
+            name: 'Game 2',
+            description: 'Description 2',
+            imageURL:
+              'https://images.igdb.com/igdb/image/upload/t_cover_big/co4a7a.png',
+            bannerURL:
+              'https://images.igdb.com/igdb/image/upload/t_cover_big/co4a7a.png',
+            tags: ['4D', 'Soullike'],
+            genres: ['Role Playing', 'Straitagy'],
+            platforms: ['Xbox 360', 'Playstation 3'],
+            releaseDate: '2021-01-02 00:00:00',
+            avgScore: 10,
+            totalRating: 5,
+            isGameAdded: false,
+            isGameLiked: false,
+          },
+          {
+            __typename: 'Game',
+            id: '3',
+            name: 'Game 3',
+            description: 'Description 3',
+            imageURL:
+              'https://images.igdb.com/igdb/image/upload/t_cover_big/co4a7a.png',
+            bannerURL:
+              'https://images.igdb.com/igdb/image/upload/t_cover_big/co4a7a.png',
+            tags: ['2D', 'Action'],
+            genres: ['JRPG', 'Simulation'],
+            platforms: ['Xbox', 'Playstation 2'],
+            releaseDate: '2021-01-03 00:00:00',
+            avgScore: 8,
+            totalRating: 5,
+            isGameAdded: false,
+            isGameLiked: false,
+          },
+        ],
+      },
+    },
+  },
+  {
+    request: {
+      query: GET_GAME_FILTERS,
+    },
+    result: {
+      data: {
+        getGameFilters: {
+          tags: ['3D', 'Fantasy'],
+          genres: ['Action', 'Adventure'],
+          platforms: ['PC', 'macOS'],
+          year: 2021,
+          errors: [],
+        },
+      },
+    },
+  },
+];
 
 vi.mock('antd', async () => {
   const actual: unknown = await vi.importActual('antd');
@@ -44,16 +124,20 @@ vi.mock('antd', async () => {
   };
 });
 
-describe.skip('Filters Wrapper Component', () => {
+describe('Filters Wrapper Component', () => {
   it('Render Filters Wrapper Component in Tablet Screen', async () => {
-    const mockFunc = vi.fn();
-    const { queryByText, queryAllByText } = render(
-      <ContextWrapper>
+    const { queryByText, queryAllByText, debug } = renderVite(
+      <DefaultMockedProvider mocks={mocks}>
         <FiltersWrapper
         // setTagsArr={mockFunc}
         />
-      </ContextWrapper>
+      </DefaultMockedProvider>,
+      {
+        store,
+      }
     );
+
+    debug();
 
     expect(queryAllByText('Genre')[0]).toBeInTheDocument();
 
