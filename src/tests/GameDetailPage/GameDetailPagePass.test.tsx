c37581a33baac10a55d27cb98d9df95475b14f8b
javascriptsucks <evix1988@live.cn>
2023-07-07 00:19:52 -0600

    update GameDetail pass test

--- src/tests/GameDetailPage/GameDetailPagePass.test.tsx
+++ src/tests/GameDetailPage/GameDetailPagePass.test.tsx
@@ -1,11 +1,46 @@
 import { describe, it, vi } from 'vitest';
-import { render, waitFor } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 
 import userEvent from '@testing-library/user-event';
 import GameDetail from '@/pages/GameDetail';
-import ContextWrapper from '@/ContextWrapper';
+import { DefaultMockedProvider, renderVite } from '@/utils/test-utils';
+import { GET_GAME_BY_ID } from '@/services/game/queries';
+import { store } from '@/app/store';
 
-describe.skip('Game Detail Page', () => {
+const mocks = [
+  {
+    request: {
+      query: GET_GAME_BY_ID,
+      variables: {
+        id: '2',
+      },
+    },
+    result: {
+      data: {
+        getGameById: {
+          __typename: 'Game',
+          id: '2',
+          name: 'Game 2',
+          description: 'Description 2',
+          imageURL:
+            'https://images.igdb.com/igdb/image/upload/t_cover_big/co4a7a.png',
+          bannerURL:
+            'https://images.igdb.com/igdb/image/upload/t_cover_big/co4a7a.png',
+          tags: ['3D', 'Fantasy'],
+          platforms: ['PC'],
+          genres: ['Action'],
+          releaseDate: '2021-01-01 00:00:00',
+          totalRating: 15,
+          avgScore: 5,
+          isGameLiked: false,
+          isGameAdded: false,
+        },
+      },
+    },
+  },
+];
+
+describe('Game Detail Page', () => {
   it('should render the game detail page', async () => {
     vi.mock('react-router-dom', async () => {
       const actual: unknown = await vi.importActual('react-router-dom');
@@ -17,58 +52,16 @@ describe.skip('Game Detail Page', () => {
       };
     });
 
-    vi.mock('../services/game/useGetGameById', async () => {
-      const actual: unknown = await vi.importActual(
-        '../services/game/useGetGameById'
-      );
-      if (typeof actual !== 'object')
-        throw new Error('Import Actual did not return not an object');
-      return {
-        ...actual,
-        default: () => ({
-          game: {
-            __typename: 'Game',
-            id: '1',
-            name: 'Game 1',
-            description: 'Description 1',
-            imageURL:
-              'https://images.igdb.com/igdb/image/upload/t_cover_big/co4a7a.png',
-            tags: ['3D', 'Fantasy'],
-            releaseDate: '2021-01-01 00:00:00',
-            avgScore: 5,
-            isGameLiked: false,
-            isGameAdded: false,
-          },
-
-          error: 'null',
-
-          getGame: vi.fn(),
-          getGameFromFragment: vi.fn((id) => {
-            return {
-              __typename: 'Game',
-              id,
-              name: `Game ${id}`,
-              description: `Description ${id}`,
-              imageURL:
-                'https://images.igdb.com/igdb/image/upload/t_cover_big/co4a7a.png',
-              tags: ['3D', 'Fantasy'],
-              releaseDate: '2021-01-01 00:00:00',
-              avgScore: 5,
-              isGameLiked: false,
-              isGameAdded: false,
-            };
-          }),
-        }),
-      };
-    });
-
-    const { queryByText, queryByLabelText, debug } = render(
-      <ContextWrapper>
+    const { queryByText, queryByLabelText, debug } = await renderVite(
+      <DefaultMockedProvider mocks={mocks}>
         <GameDetail />
-      </ContextWrapper>
+      </DefaultMockedProvider>,
+      { store }
     );
-    expect(queryByText('Game 2')).toBeInTheDocument();
-    expect(queryByText('Description 2')).toBeInTheDocument();
+
+    expect(await screen.findByText('Game 2')).toBeInTheDocument();
+    expect(await screen.findByText('Description 2')).toBeInTheDocument();
+
     const downCircleButton = queryByLabelText('down-circle') as HTMLElement;
 
     await userEvent.click(downCircleButton);
