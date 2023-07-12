9c31422c37c32ff9a2a66f2a49e2cf33d7f35924
Yzma <10090913+Yzma@users.noreply.github.com>
2023-07-12 11:34:57 -0400

    correct AllGamesHook-Fail test

--- src/tests/Game/AllGamesHook-Fail.test.tsx
+++ src/tests/Game/AllGamesHook-Fail.test.tsx
@@ -1,4 +1,4 @@
-import { describe, it, vi } from 'vitest';
+import { describe, it } from 'vitest';
 import { renderHook, waitFor } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { MockedProvider } from '@apollo/client/testing';
@@ -28,62 +28,7 @@ const mocks = [
     },
     result: {
       data: {
-        allGames: [
-          {
-            __typename: 'Game',
-            id: '1',
-            name: 'Game 1',
-            description: 'Description 1',
-            imageURL:
-              'https://images.igdb.com/igdb/image/upload/t_cover_big/co4a7a.png',
-            bannerURL:
-              'https://images.igdb.com/igdb/image/upload/t_cover_big/co4a7a.png',
-            tags: ['3D', 'Fantasy'],
-            genres: ['Action', 'Adventure'],
-            platforms: ['PC', 'macOS'],
-            releaseDate: '2021-01-01 00:00:00',
-            avgScore: 5,
-            totalRating: 5,
-            isGameAdded: false,
-            isGameLiked: false,
-          },
-          {
-            __typename: 'Game',
-            id: '2',
-            name: 'Game 2',
-            description: 'Description 2',
-            imageURL:
-              'https://images.igdb.com/igdb/image/upload/t_cover_big/co4a7a.png',
-            bannerURL:
-              'https://images.igdb.com/igdb/image/upload/t_cover_big/co4a7a.png',
-            tags: ['4D', 'Soullike'],
-            genres: ['Role Playing', 'Straitagy'],
-            platforms: ['Xbox 360', 'Playstation 3'],
-            releaseDate: '2021-01-02 00:00:00',
-            avgScore: 10,
-            totalRating: 5,
-            isGameAdded: false,
-            isGameLiked: false,
-          },
-          {
-            __typename: 'Game',
-            id: '3',
-            name: 'Game 3',
-            description: 'Description 3',
-            imageURL:
-              'https://images.igdb.com/igdb/image/upload/t_cover_big/co4a7a.png',
-            bannerURL:
-              'https://images.igdb.com/igdb/image/upload/t_cover_big/co4a7a.png',
-            tags: ['2D', 'Action'],
-            genres: ['JRPG', 'Simulation'],
-            platforms: ['Xbox', 'Playstation 2'],
-            releaseDate: '2021-01-03 00:00:00',
-            avgScore: 8,
-            totalRating: 5,
-            isGameAdded: false,
-            isGameLiked: false,
-          },
-        ],
+        allGames: [],
       },
     },
   },
