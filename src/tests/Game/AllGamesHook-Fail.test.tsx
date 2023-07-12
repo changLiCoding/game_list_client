ebcf33426b6f892e4944d63a32b3f15143e6d07a
Yzma <10090913+Yzma@users.noreply.github.com>
2023-07-11 21:11:36 -0400

    Refactor tests to pass

--- src/tests/Game/AllGamesHook-Fail.test.tsx
+++ src/tests/Game/AllGamesHook-Fail.test.tsx
@@ -1,47 +1,115 @@
 import { describe, it, vi } from 'vitest';
-import { renderHook } from '@testing-library/react';
+import { renderHook, waitFor } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { MockedProvider } from '@apollo/client/testing';
+import { ConfigProvider } from 'antd';
+import { BrowserRouter } from 'react-router-dom';
 import useAllGames from '@/services/games/useAllGames';
+import { store } from '@/app/store';
+import { GET_ALL_GAMES } from '@/services/games/queries';
 
-vi.mock('@apollo/client', async () => {
-  const actual: unknown = await vi.importActual('@apollo/client');
-  if (typeof actual !== 'object')
-    throw new Error('Import Actual did not return not an object');
-  return {
-    ...actual,
-    useQuery: vi.fn(() => ({
-      loading: false,
-      data: {},
-    })),
-  };
-});
-
-vi.mock('react-redux', async () => {
-  const actual: unknown = await vi.importActual('react-redux');
-  if (typeof actual !== 'object')
-    throw new Error('Import Actual did not return not an object');
-  return {
-    ...actual,
-    useDispatch: vi.fn(() => vi.fn()),
-  };
-});
-
-vi.mock('@/app/hooks', async () => {
-  const actual: unknown = await vi.importActual('@/app/hooks');
-  if (typeof actual !== 'object')
-    throw new Error('Import Actual did not return not an object');
-  return {
-    ...actual,
-    useAppSelector: vi.fn(() => ({
-      addedGames: {
-        addedList: [],
+const mocks = [
+  {
+    request: {
+      query: GET_ALL_GAMES,
+      variables: {
+        genre: [],
+        tag: [],
+        platform: [],
+        year: undefined,
+        excludedGenres: [],
+        excludedTags: [],
+        excludedPlatforms: [],
+        sortBy: 'name',
+        search: '',
+        limit: 20,
+        offset: 0,
       },
-    })),
-  };
-});
+    },
+    result: {
+      data: {
+        allGames: [
+          {
+            __typename: 'Game',
+            id: '1',
+            name: 'Game 1',
+            description: 'Description 1',
+            imageURL:
+              'https://images.igdb.com/igdb/image/upload/t_cover_big/co4a7a.png',
+            bannerURL:
+              'https://images.igdb.com/igdb/image/upload/t_cover_big/co4a7a.png',
+            tags: ['3D', 'Fantasy'],
+            genres: ['Action', 'Adventure'],
+            platforms: ['PC', 'macOS'],
+            releaseDate: '2021-01-01 00:00:00',
+            avgScore: 5,
+            totalRating: 5,
+            isGameAdded: false,
+            isGameLiked: false,
+          },
+          {
+            __typename: 'Game',
+            id: '2',
+            name: 'Game 2',
+            description: 'Description 2',
+            imageURL:
+              'https://images.igdb.com/igdb/image/upload/t_cover_big/co4a7a.png',
+            bannerURL:
+              'https://images.igdb.com/igdb/image/upload/t_cover_big/co4a7a.png',
+            tags: ['4D', 'Soullike'],
+            genres: ['Role Playing', 'Straitagy'],
+            platforms: ['Xbox 360', 'Playstation 3'],
+            releaseDate: '2021-01-02 00:00:00',
+            avgScore: 10,
+            totalRating: 5,
+            isGameAdded: false,
+            isGameLiked: false,
+          },
+          {
+            __typename: 'Game',
+            id: '3',
+            name: 'Game 3',
+            description: 'Description 3',
+            imageURL:
+              'https://images.igdb.com/igdb/image/upload/t_cover_big/co4a7a.png',
+            bannerURL:
+              'https://images.igdb.com/igdb/image/upload/t_cover_big/co4a7a.png',
+            tags: ['2D', 'Action'],
+            genres: ['JRPG', 'Simulation'],
+            platforms: ['Xbox', 'Playstation 2'],
+            releaseDate: '2021-01-03 00:00:00',
+            avgScore: 8,
+            totalRating: 5,
+            isGameAdded: false,
+            isGameLiked: false,
+          },
+        ],
+      },
+    },
+  },
+];
 
 describe('useAllGames', () => {
   it('returns games and loading state', async () => {
-    const { result } = renderHook(() => useAllGames());
-    expect(result.current.errors).toEqual(['No games found']);
+    const wrapper = ({ children }) => {
+      return (
+        <Provider store={store}>
+          <MockedProvider mocks={mocks}>
+            <ConfigProvider>
+              <BrowserRouter>{children}</BrowserRouter>
+            </ConfigProvider>
+          </MockedProvider>
+        </Provider>
+      );
+    };
+
+    const { result } = renderHook(() => useAllGames(), {
+      wrapper,
+    });
+
+    await waitFor(() => {
+      expect(result.current.errors).toEqual(['No games found']);
+      expect(result.current.games).toEqual([]);
+    });
   });
 });
